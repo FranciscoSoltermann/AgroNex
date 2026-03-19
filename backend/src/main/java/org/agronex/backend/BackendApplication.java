524fccb1fd75a106b8920344f7754258163f595b
Francisco Soltermann <frasoltermann@gmail.com>
2026-03-19 08:01:49 -0300

    agregado de seguridad en conexion backend-BDD

--- backend/src/main/java/org/agronex/backend/BackendApplication.java
+++ backend/src/main/java/org/agronex/backend/BackendApplication.java
@@ -1,5 +1,6 @@
 package org.agronex.backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		// Cargamos el archivo .env
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+		// Seteamos las variables como propiedades del sistema para que Spring las lea
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
 }
