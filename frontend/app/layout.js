31acd1359bff801434c078a2a3a0a0b115b1efd3
Francisco Soltermann <frasoltermann@gmail.com>
2026-04-02 11:20:54 -0300

    eliminado AI

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import FooterMount from "@/components/layout/FooterMount";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <Toaster richColors position="top-right" />
         {children}
         <FooterMount />
       </body>
