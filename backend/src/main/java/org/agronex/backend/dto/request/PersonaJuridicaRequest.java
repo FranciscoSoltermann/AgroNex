2d6a852f82ebf02a7032e70d3bd74876af698c11
Bautista Luetich <bautistaluetich@gmail.com>
2026-08-12 10:12:09 -0300

    ajustes de front en el inicio

--- backend/src/main/java/org/agronex/backend/dto/request/PersonaJuridicaRequest.java
+++ backend/src/main/java/org/agronex/backend/dto/request/PersonaJuridicaRequest.java
@@ -8,20 +8,22 @@ import lombok.NoArgsConstructor;
 
 @Data
 @Builder
-@NoArgsConstructor  // <--- AGREGAR ESTO
-@AllArgsConstructor // <--- AGREGAR ESTO
+@NoArgsConstructor
+@AllArgsConstructor
 public class PersonaJuridicaRequest {
     @NotBlank(message = "El email no puede estar vacío")
     @Email(message = "Debe ser un formato de email válido")
+    @Size(max = 254, message = "El email es demasiado largo")
     private String email;
 
 
 
     @NotBlank(message = "La razón social es obligatoria")
+    @Size(min = 2, max = 100, message = "La razón social debe tener entre 2 y 100 caracteres")
     private String razonSocial;
 
     @NotBlank(message = "El CUIT es obligatorio")
-    @Pattern(regexp = "\\d+", message = "El CUIT solo debe contener números")
+    @Pattern(regexp = "\\d{11}", message = "El CUIT debe tener exactamente 11 dígitos numéricos")
     private String cuit;
 
     private org.agronex.backend.enums.RolUsuario rol;
